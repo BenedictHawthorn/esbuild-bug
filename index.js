5ec4eb77c96bd01902d8c8e7df960726a226effc
BenedictHawthorn <benedict.hawthorn@squaredup.com>
2021-11-03 11:05:15 +0000

    Require

--- index.js
+++ index.js
@@ -1,2 +1,2 @@
-import ac from './node_modules/adal-node/lib/authentication-context.js';
+const ac = require('./node_modules/adal-node/lib/authentication-context.js');
 console.log(new ac.AuthenticationContext());
